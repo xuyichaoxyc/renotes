3f0d78620f7d3c7e88b21bff60dbb3114618eb3d
xuyichaoxyc <1817854647@qq.com>
2021-06-29 19:04:37 +0800

    守护线程和用户线程

---
+++ Projects/MultiThread/part01-ThreadBases/src/thread_daemon/DaemonThreadTest.java
@@ -0,0 +1,21 @@
+package thread_daemon;
+
+/**
+ * @author ：xuyichao
+ * @description：
+ * @date ：2021/6/29 18:57
+ */
+public class DaemonThreadTest {
+    public static void main(String[] args) {
+        Thread daemonThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+        // 设置为守护线程
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+    }
+}
---
+++ Projects/MultiThread/part01-ThreadBases/src/thread_daemon/DaemonThreadTest2.java
@@ -0,0 +1,25 @@
+package thread_daemon;
+
+/**
+ * @author ：xuyichao
+ * @description：
+ * @date ：2021/6/29 18:57
+ */
+public class DaemonThreadTest2 {
+    public static void main(String[] args) {
+        Thread daemonThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (; ; ) {
+
+                }
+            }
+        });
+
+        // 设置为守护线程
+        // daemonThread.setDaemon(true);
+        daemonThread.start();
+
+        System.out.println("main is over.");
+    }
+}
