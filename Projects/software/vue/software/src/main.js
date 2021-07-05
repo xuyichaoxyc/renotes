8acd681dba52be420e0bac244d267e1fbd58e33b
binghambai <binghambai@hz.com>
2021-07-05 19:19:00 +0800

    增加了index.vue，有了基本的布局（未完成）

--- Projects/software/vue/software/src/main.js
+++ Projects/software/vue/software/src/main.js
@@ -4,7 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//引入ant-design
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+Vue.use(Antd);
+
 Vue.config.productionTip = false
+
+
 
 /* eslint-disable no-new */
 new Vue({
