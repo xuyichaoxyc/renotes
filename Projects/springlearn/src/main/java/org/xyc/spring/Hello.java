56393c17e46787bb63163a9858b72897e9b3342b
xuyichaoxyc <1817854647@qq.com>
2021-07-06 08:58:33 +0800

    第一个 Spring 示例项目

---
+++ Projects/springlearn/src/main/java/org/xyc/spring/Hello.java
@@ -0,0 +1,12 @@
+package org.xyc.spring;
+
+/**
+ * @author ：xuyichao
+ * @description：
+ * @date ：2021/7/6 8:54
+ */
+public class Hello {
+    public void say() {
+        System.out.println("Hello 类的 say 方法执行......");
+    }
+}
