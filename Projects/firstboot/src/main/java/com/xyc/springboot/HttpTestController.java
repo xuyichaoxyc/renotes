4d1866e80ff896c2697a9fccddca4c4c45e0ede1
xuyichaoxyc <1817854647@qq.com>
2021-07-09 09:41:33 +0800

    SpringBoot2精髓-使用基础.md

--- Projects/firstboot/src/main/java/com/xyc/springboot/HttpTestController.java
+++ Projects/firstboot/src/main/java/com/xyc/springboot/HttpTestController.java
@@ -1,4 +1,4 @@
-package com.xyc.springboot.controller;
+package com.xyc.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- Projects/firstboot/src/main/java/com/xyc/springboot/controller/RestTestController.java
+++ Projects/firstboot/src/main/java/com/xyc/springboot/controller/RestTestController.java
@@ -1,4 +1,4 @@
-package com.xyc.springboot;
+package com.xyc.springboot.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
