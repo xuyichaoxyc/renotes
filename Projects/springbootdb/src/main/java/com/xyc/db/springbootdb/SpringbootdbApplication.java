aa266561934f172401b46e85c7b9a033fde46b0e
xuyichaoxyc <1817854647@qq.com>
2021-07-10 21:28:57 +0800

    springbootdb

---
+++ Projects/springbootdb/src/main/java/com/xyc/db/springbootdb/SpringbootdbApplication.java
@@ -0,0 +1,13 @@
+package com.xyc.db.springbootdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdbApplication.class, args);
+    }
+
+}
