b0229441bfbc8efd172a63ff429ea9eea139dcf0
xuyichaoxyc <1817854647@qq.com>
2021-07-16 17:20:53 +0800

    1. 配置 pom 依赖 2. 书写配置文件 3. 创建sql

---
+++ Projects/springboot-vueblog/src/main/java/com/xyc/vueblog/springbootvueblog/SpringbootVueblogApplication.java
@@ -0,0 +1,13 @@
+package com.xyc.vueblog.springbootvueblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVueblogApplication.class, args);
+    }
+
+}
---
+++ Projects/springboot-vueblog/src/test/java/com/xyc/vueblog/springbootvueblog/SpringbootVueblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyc.vueblog.springbootvueblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVueblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
