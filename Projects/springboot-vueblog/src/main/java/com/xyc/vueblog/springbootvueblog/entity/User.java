d9af671b5c4cc3bbc3a70f908dee0d7fbbc929e6
xuyichaoxyc <1817854647@qq.com>
2021-07-16 17:31:32 +0800

    -

---
+++ Projects/springboot-vueblog/src/main/java/com/xyc/vueblog/springbootvueblog/entity/User.java
@@ -0,0 +1,12 @@
+package com.xyc.vueblog.springbootvueblog.entity;
+
+/**
+ * @ClassName User
+ * @Description 用户实体类
+ * @Author admin-xuyichao
+ * @Date 2021/7/16 17:21
+ * @Version 1.0
+ **/
+public class User {
+
+}
