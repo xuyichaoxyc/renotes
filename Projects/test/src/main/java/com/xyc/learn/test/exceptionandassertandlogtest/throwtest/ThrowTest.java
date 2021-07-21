a4911f796cb9b17ca032ee1bee5d654607aa5812
xuyichaoxyc <1817854647@qq.com>
2021-07-21 15:12:24 +0800

    异常抛出测试

---
+++ Projects/test/src/main/java/com/xyc/learn/test/exceptionandassertandlogtest/throwtest/ThrowTest.java
@@ -0,0 +1,18 @@
+package com.xyc.learn.test.exceptionandassertandlogtest.throwtest;
+
+/**
+ * @ClassName ThrowTest
+ * @Description TODO
+ * @Author admin-xuyichao
+ * @Date 2021/7/21 14:31
+ * @Version 1.0
+ **/
+public class ThrowTest {
+    public static void main(String[] args) {
+        int resultNormal = RandomGenerator.randInt(2, 5);
+        System.out.println("正常输出随机生成结果：" + resultNormal);
+
+        int resultException = RandomGenerator.randInt(5, 2);
+        System.out.println("发生异常" + resultException);
+    }
+}
